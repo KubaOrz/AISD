d2d597482580eb9e4c5a42d62aa44137e84f4c6c
KubaOrz <orz.kub@wp.pl>
2022-10-21 19:06:53 +0200

    Implementacja kopca i testy do niego

--- aisd_lab_1_sorting/src/main/java/pl/edu/pw/ee/Heap.java
+++ aisd_lab_1_sorting/src/main/java/pl/edu/pw/ee/Heap.java
@@ -1,5 +1,6 @@
 package pl.edu.pw.ee;
 
+import java.util.ArrayList;
 import java.util.List;
 import pl.edu.pw.ee.services.HeapExtension;
 import pl.edu.pw.ee.services.HeapInterface;
@@ -7,25 +8,57 @@ import pl.edu.pw.ee.services.HeapInterface;
 public class Heap<T extends Comparable<T>> implements HeapInterface<T>, HeapExtension {
 
     private List<T> data;
+    private int currentSize;
 
     public Heap(List<T> data) {
         this.data = data;
+        currentSize = data.size();
+        this.build();
+    }
+
+    public Heap() {
+        this.data = new ArrayList<>();
+        this.currentSize = 0;
     }
 
     @Override
     public void put(T item) {
         data.add(item);
-        int index = data.size();
-        while (item.compareTo(data.get((index - 1) / 2)) > 0) {
-            
+        heapUp(item);
+        currentSize++;
+    }
+
+    public int getCurrentSize() {
+        return currentSize;
+    }
+
+    public void heapUp(T item) {
+        int index = data.size() - 1;
+        int parent = (index - 1) / 2;
+        while (index > 0 && (item.compareTo(data.get(parent)) > 0)) {
+            swap(index, parent);
+            index = parent;
+            parent = (index - 1) / 2;
         }
+    }
+
+    private void swap(int firstId, int secondId) {
+        T tmp = data.get(firstId);
+        data.set(firstId, data.get(secondId));
+        data.set(secondId, tmp);
     }
 
     @Override
     public T pop() {
-        // TODO
+        if (currentSize == 0) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        T maxValue = data.get(0);
+        swap(0, --currentSize);
+        heapify(0, currentSize);
+        return maxValue;
     }
-
+    
     @Override
     public void build() {
         int n = data.size();
@@ -37,7 +70,25 @@ public class Heap<T extends Comparable<T>> implements HeapInterface<T>, HeapExte
 
     @Override
     public void heapify(int startId, int endId) {
-        // TODO
+        int left = 2 * startId + 1;
+        int right = 2 * startId + 2;
+        int largest = startId;
+        int tmp = startId;
+        while (true) {
+            if (left < endId && data.get(left).compareTo(data.get(largest)) > 0) {
+                largest = left;
+            }
+            if (right < endId && data.get(right).compareTo(data.get(largest)) > 0) {
+                largest = right;
+            }
+            if (largest == tmp) {
+                return;
+            }
+            swap(largest, tmp);
+            tmp = largest;
+            left = 2 * tmp + 1;
+            right = 2 * tmp + 2;
+        }
     }
 
 }
--- aisd_lab_1_sorting/src/test/java/pl/edu/pw/ee/HeapTest.java
+++ aisd_lab_1_sorting/src/test/java/pl/edu/pw/ee/HeapTest.java
@@ -1,5 +1,176 @@
 package pl.edu.pw.ee;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HeapTest {
+
+    private Heap<Double> heap;
+    private Heap<Double> heapFromArray;
+    private static final int delta = 0;
+
+    @Before
+    public void setUp() {
+        heap = new Heap<>();
+        double[] nums = {1, 6, 4, 7, 9, 10, 21, -3, -8};
+        List<Double> numsToBuild = new ArrayList<>();
+        for (Double num: nums) {
+            numsToBuild.add(num);
+        }
+        heapFromArray = new Heap<>(numsToBuild);
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void shouldThrowExceptionWhenEmpty() {
+
+        //when
+        heap.pop();
+
+        //then
+        assert false;
+    }
+
+    @Test
+    public void shouldReturnProperSizeWhenInit() {
+
+        Assert.assertEquals(0, heap.getCurrentSize());
+    }
+
+    @Test
+    public void shouldReturnProperSizeWHenPutSomeElem() {
+
+        //when
+        heap.put(1.1);
+        heap.put(1.2);
+        heap.put(1.3);
+        int expected = 3;
+
+        //then
+        Assert.assertEquals(expected, heap.getCurrentSize());
+    }
+
+    @Test
+    public void shouldReturnProperSizeWHenPutAndPopSomeElem() {
+
+        //when
+        heap.put(1.1);
+        heap.put(1.2);
+        heap.put(1.3);
+        heap.pop();
+        heap.pop();
+        int expected = 1;
+
+        //then
+        Assert.assertEquals(expected, heap.getCurrentSize());
+    }
+
+    @Test
+    public void shouldReturnElemWhenPutOneElem() {
+
+        //given
+        heap.put(2.137);
+
+        //when
+        double result = heap.pop();
+        double expected = 2.137;
+
+        //then
+        Assert.assertEquals(expected, result, delta);
+    }
+
+    @Test
+    public void shouldReturnMaxElemWhenPutTwoElem() {
+        //given
+        heap.put(2.137);
+        heap.put(2.115);
+
+        //when
+        double result = heap.pop();
+        double expected = 2.137;
+
+        //then
+        Assert.assertEquals(expected, result, delta);
+    }
+
+    @Test
+    public void shouldReturnMaxElemWhenPutSomeElem() {
+        //given
+        heap.put(2.137);
+        heap.put(3.52);
+        heap.put(21.0);
+        heap.put(3246.1);
+        heap.put(1.1);
+        heap.put(2.2);
+        heap.put(3.3);
+        heap.put(4.4);
+
+        //when
+        double result = heap.pop();
+        double expected = 3246.1;
+
+        //then
+        Assert.assertEquals(expected, result, delta);
+    }
+
+    @Test
+    public void shouldReturnCorrectOrderWhenPutAndPopSomeElem() {
+
+        //given
+        heap.put(1.1);
+        heap.put(2.1);
+        heap.put(5.1);
+        heap.put(3.1);
+        heap.put(-1.1);
+        heap.put(21.1);
+
+        //when
+        double[] results = new double[6];
+        double[] expected = {21.1, 5.1, 3.1, 2.1, 1.1, -1.1};
+        for (int i = 0; i < 6; i++) {
+            results[i] = heap.pop();
+        }
+
+        //then
+        Assert.assertArrayEquals(expected, results, delta);
+    }
+
+    @Test
+    public void shouldReturnProperSizeWhenBuilt() {
+
+        //when
+        int expected = 9;
+
+        //then
+        Assert.assertEquals(expected, heapFromArray.getCurrentSize());
+    }
+
+    @Test
+    public void shouldReturnMaxElemWhenBuilt() {
+
+        //when
+        double expected = 21;
+        double result = heapFromArray.pop();
+
+        //then
+        Assert.assertEquals(expected, result, delta);
+    }
+
+    @Test
+    public void shouldReturnProperOrderWhenBuilt() {
+
+        //when
+        double[] results = new double[9];
+        double[] expected = {21, 10, 9, 7, 6, 4, 1, -3, -8};
+        for (int i = 0; i < 9; i++) {
+            results[i] = heapFromArray.pop();
+        }
+
+        //then
+        Assert.assertArrayEquals(expected, results, delta);
+    }
 
 }
